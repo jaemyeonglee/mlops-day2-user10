705d1dff7f7e53d69251a63bc6d29064d4db9bec
ljm <ljm@sk.com>
2025-08-28 06:26:58 +0000

    chore: workspace hygiene (Ruff formatOnSave, trim whitespace, final newline)

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-import pandas as pd, matplotlib.pyplot as plt
-df = pd.DataFrame({"x":[1,2,3], "y":[2,4,3]})
-df.plot(x="x", y="y"); plt.show()
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.DataFrame({"x": [1, 2, 3], "y": [2, 4, 3]})
+df.plot(x="x", y="y")
+plt.show()
